9ce1d7b3606e5fafa3532cfea8efdd1ac6efeb5c
Riley Lin <75247434+FakeStandard@users.noreply.github.com>
2022-10-28 11:56:48 +0800

    add model of daily quotes

---
+++ SMAJ-Model/DailyQuotes.cs
@@ -0,0 +1,101 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SMAJ_Model
+{
+    public class DailyQuotes
+    {
+        /// <summary>
+        /// PK
+        /// </summary>
+        [Key]
+        [Column("PK")]
+        public int ID { get; set; }
+
+        /// <summary>
+        /// 證券代號
+        /// </summary>
+        public string StockID { get; set; }
+
+        /// <summary>
+        /// 證券名稱
+        /// </summary>
+        public string StockName { get; set; }
+
+        /// <summary>
+        /// 交易日期
+        /// </summary>
+        public DateTime TradeDate { get; set; }
+
+        /// <summary>
+        /// 成交股數
+        /// </summary>
+        public string TradeVolume { get; set; }
+
+        /// <summary>
+        /// 成交筆數
+        /// </summary>
+        public string TradeValue { get; set; }
+
+        /// <summary>
+        /// 成交金額
+        /// </summary>
+        public string TradeAmount { get; set; }
+
+        /// <summary>
+        /// 開盤價
+        /// </summary>
+        public string OpeningPrice { get; set; }
+
+        /// <summary>
+        /// 最高價
+        /// </summary>
+        public string HighestPrice { get; set; }
+
+        /// <summary>
+        /// 最低價
+        /// </summary>
+        public string LowestPrice { get; set; }
+
+        /// <summary>
+        /// 收盤價
+        /// </summary>
+        public string ClosingPrice { get; set; }
+
+        /// <summary>
+        /// 漲跌(+/-)
+        /// </summary>
+        public string Dir { get; set; }
+
+        /// <summary>
+        /// 漲跌價差
+        /// </summary>
+        public string Change { get; set; }
+
+        /// <summary>
+        /// 最後揭示買價
+        /// </summary>
+        public string LastBestBidPrice { get; set; }
+
+        /// <summary>
+        /// 最後揭示買量
+        /// </summary>
+        public string LastBestBidVolume { get; set; }
+
+        /// <summary>
+        /// 最後揭示賣價
+        /// </summary>
+        public string LastBestAskPrice { get; set; }
+
+        /// <summary>
+        /// 最後揭示賣量
+        /// </summary>
+        public string LastBestAskVolume { get; set; }
+
+        /// <summary>
+        /// 本益比 PER
+        /// </summary>
+        [Column("PriceEarningRatio")]
+        public string PER { get; set; }
+    }
+}
---
+++ SMAJ-Model/DailyQuotesInfo.cs
@@ -0,0 +1,15 @@
+﻿
+namespace SMAJ_Model
+{
+    /// <summary>
+    /// 每日收盤資訊 JSON 對應欄位
+    /// </summary>
+    public class DailyQuotesInfo
+    {
+        public string stat { get; set; }
+
+        public List<string> field9 { get; set; }
+
+        public List<List<string>> data9 { get; set; }
+    }
+}
